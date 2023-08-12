d6f3d109671f8e2b5723738f01ec817eed449198
Arjun Maudhoo <apm526@nyu.edu>
2023-08-12 17:03:15 -0400

    Add client

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Post from './Post';
+import Header from './Header';
+import { Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+        <Route index element={} />
+    </Routes>
+    <main>
+      <Header />
+      <Post />
+      <Post />
+      <Post />
+    </main>
+  );
+}
+
+export default App;
