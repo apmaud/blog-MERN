f1acb6a446b275cc112bf33e8ad62ce2934d292e
Arjun Maudhoo <apm526@nyu.edu>
2023-08-16 21:56:28 -0500

    Begin API development

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import Post from './Post';
 import Header from './Header';
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <Routes>
-        <Route index element={} />
+        <Route path="/" element ={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+        </Route>
     </Routes>
-    <main>
-      <Header />
-      <Post />
-      <Post />
-      <Post />
-    </main>
   );
 }
 
