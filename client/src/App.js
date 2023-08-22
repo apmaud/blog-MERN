79ac1cef088d30584c3fa3c7e5fe151fdb034ab9
Arjun Maudhoo <apm526@nyu.edu>
2023-08-22 17:20:51 -0500

    Finish functional blog!

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import CreatePost from './pages/CreatePost';
 import PostPage from './pages/PostPage';
+import EditPost from './pages/EditPost';
 import { UserContextProvider } from './UserContext';
 
 function App() {
@@ -17,7 +18,8 @@ function App() {
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
           <Route path='/create' element={<CreatePost />} />
-          <Route path ='/post/:id' element ={<PostPage />} />
+          <Route path='/post/:id' element={<PostPage />} />
+          <Route path='/edit/:id' element={<EditPost />} />
         </Route>
       </Routes>
     </UserContextProvider>
--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,22 +1,6 @@
 import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-const modules = {
-    toolbar: [
-        [{ 'header': [1, 2, false] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-        ['link', 'image'],
-        ['clean']
-    ]
-};
-const formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image'
-];
+import Editor from '../Editor.js';
 
 export default function CreatePost() {
     const [title, setTitle] = useState('');
@@ -57,11 +41,7 @@ export default function CreatePost() {
                     onChange={ev => setSummary(ev.target.value)} />
             <input  type="file"
                     onChange={ev => setFiles(ev.target.files)} />
-            <ReactQuill 
-                value={content} 
-                onChange={newValue => setContent(newValue)}
-                modules={modules} 
-                formats={formats} />
+            <Editor onChange={setContent} value={content} />
             <button style={{marginTop:'5px'}}>Create post</button>
         </form>
     );
